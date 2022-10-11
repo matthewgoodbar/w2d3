ab5a7aeb79f4b47f932302339291555e08dac327
matthew <matthewgoodbar@gmail.com>
2022-10-11 09:23:33 -0700

    Implemented battleship_project part 3

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -3,4 +3,48 @@ require_relative "player"
 
 class Battleship
 
+  attr_reader :board, :player
+
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
+
+  def start_game
+    @board.place_random_ships
+    puts @board.num_ships
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts "you lose"
+      return true
+    else
+      return false
+    end
+  end
+
+  def win?
+    if @board.num_ships > 0
+      return false
+    else
+      puts "you win"
+      return true
+    end
+  end
+
+  def game_over?
+    return true if win? || lose?
+    return false
+  end
+
+  def turn
+    user_input = @player.get_move
+    hit = @board.attack(user_input)
+    @board.print
+    @remaining_misses -= 1 if !hit
+    puts @remaining_misses
+  end
 end
--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+  def get_move
+    puts "enter a position with coordinates separated with a space like `4 7`"
+    coords = gets.chomp.split(" ")
+    return [coords[0].to_i, coords[1].to_i]
+  end
 end
