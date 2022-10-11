d41d69dce43c27f62fbaef4ee91166e7b220a1b6
matthew <matthewgoodbar@gmail.com>
2022-10-11 11:58:36 -0700

    Implemented human_player.rb in tictactoe project

---
+++ tictactoe_project/human_player.rb
@@ -0,0 +1,24 @@
+class HumanPlayer
+
+    attr_reader :mark_value
+
+    def initialize(mark_value)
+        @mark_value = mark_value
+    end
+
+    def get_position
+        begin
+            puts @mark_value.to_s + "player, enter a position:"
+            user_input = gets.chomp.split(" ")
+            user_input.map!(&:to_i)
+            if user_input.length != 2 || (!user_input[0].is_a?(Integer) && !user_input[1].is_a?(Integer))
+                raise RuntimeError.new "Invalid Input"
+            end
+        rescue
+            puts "Invalid input! Please enter two integers, separated by a space."
+            retry
+        end
+        return user_input
+    end
+
+end
